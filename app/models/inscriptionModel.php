a341b7eca058b90bdfd64c3113bc06eace5f96d9
dootod <181655113+dootod@users.noreply.github.com>
2025-10-10 14:50:15 +0200

    Ajout de l'inscription

--- app/models/inscriptionModel.php
+++ app/models/inscriptionModel.php
@@ -5,4 +5,15 @@ require_once __DIR__ . '/../../config/database.php';
 function creerUtilisateur($nom, $prenom, $email, $mdp) {
 
     $pdo = getPDO();
+
+    $mdp_hash = password_hash($mdp, PASSWORD_DEFAULT);
+
+    $requete = $pdo->prepare("INSERT INTO utilisateur (id_utilisateur, mot_de_passe, email, nom, prenom, type_profil) VALUES (NULL, :mdp, :email, :nom, :prenom, :type)");
+    $requete->execute([
+        "mdp" => $mdp_hash,
+        "email" => $email,
+        "nom" => $nom,
+        "prenom" => $prenom,
+        "type" => "user"
+    ]);
 }
