0238e1679e3b16d5f1eb563ab03d3d396cef1694
dootod <181655113+dootod@users.noreply.github.com>
2025-12-10 14:56:35 +0100

    update

--- app/models/inscriptionModel.php
+++ app/models/inscriptionModel.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../../config/database_puy.php';
 function utilisateurExistant($email) {
     $pdo = getPDO();
     $requete = $pdo->prepare("SELECT id_utilisateur FROM utilisateur WHERE email = :email");
--- app/models/listeModel.php
+++ app/models/listeModel.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../../config/database_puy.php';
 class ListeModel {
     
     public function getElements($filtre) {
